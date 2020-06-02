f3afb28e84c5e9ec6628e82b81637f868aa5c8dd
Claudio Rivera <me@claudiorivera.com>
2020-06-02 11:54:57 -0500

    CRA cleanup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "fontsource-roboto";
+import { CssBaseline } from "@material-ui/core";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <div>App</div>
+    </div>
+  );
 }
 
 export default App;
