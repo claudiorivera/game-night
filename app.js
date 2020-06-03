24b43b3d3995b307b30f4fe31fba9109c6315e43
Claudio Rivera <me@claudiorivera.com>
2020-06-03 10:12:51 -0500

    Simplified folder structure again

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
@@ -12,7 +11,6 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(helmet());
 app.use(compression());
 
