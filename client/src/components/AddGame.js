5423771a09439b45ebc4d8aa3f59c4079cd57deb
Claudio Rivera <me@claudiorivera.com>
2020-06-13 12:58:17 -0500

    Expansion panel working. Begin fetch refactor to return array.

--- client/src/components/AddGame.js
+++ client/src/components/AddGame.js
@@ -1,26 +1,36 @@
-import React, { useState, useContext, Fragment } from "react";
+import React, { useState, useContext } from "react";
 import { GlobalContext } from "../context";
 import {
   Button,
   TextField,
   Container,
-  CircularProgress,
-  List,
-  ListItem,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Alert, AlertTitle } from "@material-ui/lab";
+import { ExpandMore as ExpandMoreIcon } from "@material-ui/icons";
 
 import { GameDetails } from "../components";
 import { bggFetchGameByQuery } from "../util/bggFetchGameByQuery";
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  alert: {
     width: "100%",
     "& > * + *": {
       marginTop: theme.spacing(2),
     },
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
   },
 }));
 
@@ -62,7 +72,7 @@ const AddGame = () => {
   return (
     <Container>
       {alert && (
-        <div className={classes.root}>
+        <div className={classes.alert}>
           <Alert
             onClose={() => {
               setAlert(null);
@@ -93,33 +103,30 @@ const AddGame = () => {
           Search
         </Button>
       </form>
-      {/* Search results begin */}
-      {queryResults.length > 0 && (
-        <List>
-          {queryResults.map((result) => (
-            <ListItem key={result.bggId}>{result.name}</ListItem>
-          ))}
-        </List>
-      )}
-
-      {/* Search results end */}
-
-      {/* Display game details if we got any */}
-      {isFetching && <CircularProgress />}
-      {!isFetching && gameDetails && gameDetails.bggId !== 0 && (
-        <Fragment>
-          <GameDetails game={gameDetails} />
-          <Button
-            size="large"
-            fullWidth
-            color="primary"
-            variant="contained"
-            onClick={handleAddGame}
-          >
-            Add This Game
-          </Button>
-        </Fragment>
-      )}
+      {queryResults.length > 0 &&
+        !isFetching &&
+        queryResults.map((result) => (
+          <ExpansionPanel key={result.bggId}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel-${result.bggId}-content`}
+            >
+              <Typography className={classes.heading}>{result.name}</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails className={classes.details}>
+              <GameDetails game={gameDetails} />
+              <Button
+                fullWidth
+                size="large"
+                color="primary"
+                variant="contained"
+                onClick={handleAddGame}
+              >
+                Add This Game
+              </Button>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        ))}
     </Container>
   );
 };
