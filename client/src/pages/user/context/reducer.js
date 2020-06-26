f013fdeaa847fba93fabed0eb3e570a3041c520a
Claudio Rivera <me@claudiorivera.com>
2020-06-26 12:28:22 -0500

    UserContext doesn't need alert reducers

--- client/src/pages/user/context/reducer.js
+++ client/src/pages/user/context/reducer.js
@@ -1,16 +1,10 @@
-export const reducer = (state, { type, message, user }) => {
+export const reducer = (state, { type, user }) => {
   switch (type) {
-    case "REGISTER_USER_FAILED_WITH_MESSAGE":
-    case "LOGIN_FAILED_WITH_MESSAGE":
-    case "LOGOUT_FAILED_WITH_MESSAGE":
-    case "DELETE_USER_BY_ID_FAILED_WITH_MESSAGE":
-      return { ...state, alert: message };
     case "REGISTER_USER_SUCCESSFUL_WITH_USER":
     case "LOGIN_SUCCESSFUL_WITH_USER":
     case "LOGOUT_SUCCESSFUL_WITH_USER":
     case "DELETE_USER_BY_ID_SUCCESSFUL_WITH_USER":
       return { ...state, user };
-
     default:
       return state;
   }
