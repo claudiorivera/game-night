95ea6ed048dca06c5ae2f10e9096fc91918cbcd2
Claudio Rivera <me@claudiorivera.com>
2020-06-30 20:25:28 -0500

    Added guest list and button display-hide logic

--- client/src/pages/events/EventDetailsPage.js
+++ client/src/pages/events/EventDetailsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import {
   Container,
@@ -8,10 +8,12 @@ import {
   CardContent,
   CardActions,
   Typography,
+  Chip,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { ArrowBack as ArrowBackIcon } from "@material-ui/icons";
 import GameDetails from "../games/components/GameDetails";
+import { UserContext } from "../user/context";
 
 const axios = require("axios").default;
 const moment = require("moment");
@@ -29,6 +31,7 @@ const EventDetailsPage = () => {
   const { eventId } = useParams();
   const history = useHistory();
   const [event, setEvent] = useState(null);
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     const getEventById = async (id) => {
@@ -54,15 +57,26 @@ const EventDetailsPage = () => {
             <Typography variant="body1">
               Hosted by: {event.host.name}
             </Typography>
-            <Typography variant="body1">
-              Guests: {event.guests.length}
+            <Typography variant="subtitle1">
+              Guests:
+              {event.guests.map((guest, index) => (
+                <Chip key={index} label={guest.name} />
+              ))}
             </Typography>
             <Typography variant="body1">Game Info:</Typography>
             <GameDetails game={event.game} />
           </CardContent>
           <CardActions>
-            <Button>Join</Button>
-            <Button>Edit</Button>
+            {/* If user is already a guest, show the Unjoin button */}
+            {event.guests.some((guest) => guest._id === user._id) ? (
+              <Button>Unjoin</Button>
+            ) : // Otherwise, as long as user isn't the host, show the Join button
+            event.host._id !== user._id ? (
+              <Button>Join</Button>
+            ) : (
+              // Otherwise, we're the host, so show the Edit button
+              <Button>Edit</Button>
+            )}
           </CardActions>
         </Card>
       )}
