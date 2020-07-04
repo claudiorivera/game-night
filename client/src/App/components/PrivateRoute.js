6bedd33c7978088a01083a90cac823f62aa80acd
Claudio Rivera <me@claudiorivera.com>
2020-07-04 17:53:48 -0500

    Added check to ensure User logged in

--- client/src/App/components/PrivateRoute.js
+++ client/src/App/components/PrivateRoute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ children, ...rest }) => {
     <Route
       {...rest}
       render={({ location }) =>
-        user ? (
+        user && user._id ? (
           children
         ) : (
           <Redirect
