e1a5c1a98fc14c6597e4e36843b1a2f2bd5c37da
Claudio Rivera <me@claudiorivera.com>
2020-07-04 20:31:48 -0500

    Finish edit event page

--- client/src/pages/events/EditEventPage.js
+++ client/src/pages/events/EditEventPage.js
@@ -9,21 +9,33 @@ import {
   MenuItem,
   Button,
 } from "@material-ui/core";
+import { ArrowBack as ArrowBackIcon } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 import { DateTimePicker } from "@material-ui/pickers";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 const EditEventPage = () => {
   const { games, getAllGames } = useContext(GamesContext);
-  const { addEvent } = useContext(EventsContext);
-  const [eventDateTime, setEventDateTime] = useState(new Date());
+  const { getEventById, updateEvent } = useContext(EventsContext);
+  const { eventId } = useParams();
+  const [eventDateTime, setEventDateTime] = useState(Date.now());
   const [gameId, setGameId] = useState("");
+  const [event, setEvent] = useState(null);
 
   useEffect(() => {
     const fetchGames = async () => {
-      await getAllGames();
+      if (!games) {
+        await getAllGames();
+      }
+    };
+    const fetchEvent = async () => {
+      const event = await getEventById(eventId);
+      setEventDateTime(event.eventDateTime);
+      setGameId(event.game._id);
+      setEvent(event);
     };
     fetchGames();
+    fetchEvent();
     //eslint-disable-next-line
   }, []);
 
@@ -38,50 +50,56 @@ const EditEventPage = () => {
 
   return (
     <Container>
-      <Typography variant="h4">Add New Event</Typography>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await addEvent(gameId, eventDateTime);
-          history.goBack();
-        }}
-      >
-        <FormControl className={classes.margin}>
-          <DateTimePicker
-            disablePast
-            id="datetime-picker"
-            minutesStep={15}
-            value={eventDateTime}
-            onChange={setEventDateTime}
-          />
-        </FormControl>
-        {games && (
-          <FormControl className={classes.margin}>
-            <Select
-              id="game-select"
-              value={gameId}
-              onChange={(e) => {
-                setGameId(e.target.value);
-              }}
-            >
-              {games.map(({ _id, name }) => (
-                <MenuItem key={_id} value={_id}>
-                  {name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        )}
-        <Button
-          size="large"
-          fullWidth
-          variant="contained"
-          color="primary"
-          type="submit"
+      <Button onClick={() => history.goBack()}>
+        <ArrowBackIcon />
+        Go Back
+      </Button>
+      <Typography variant="h4">Edit Event</Typography>
+      {event && (
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await updateEvent(eventId, gameId, eventDateTime);
+            history.goBack();
+          }}
         >
-          Add Event
-        </Button>
-      </form>
+          <FormControl className={classes.margin}>
+            <DateTimePicker
+              disablePast
+              id="datetime-picker"
+              minutesStep={15}
+              value={eventDateTime}
+              onChange={setEventDateTime}
+            />
+          </FormControl>
+          {games && (
+            <FormControl className={classes.margin}>
+              <Select
+                id="game-select"
+                value={gameId}
+                onChange={(e) => {
+                  setGameId(e.target.value);
+                }}
+              >
+                {games.map(({ _id, name }) => (
+                  <MenuItem key={_id} value={_id}>
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+          <Button
+            size="large"
+            fullWidth
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Save
+          </Button>
+        </form>
+      )}
     </Container>
   );
 };
