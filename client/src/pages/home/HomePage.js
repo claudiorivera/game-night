952adf043a1e294ba71ffe02c418a48579c2e436
Claudio Rivera <me@claudiorivera.com>
2020-07-07 10:21:40 -0500

    Fixed user check logic

--- client/src/pages/home/HomePage.js
+++ client/src/pages/home/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
   const { user, getUserEvents, getUserEventsHosting } = useContext(UserContext);
 
   useEffect(() => {
-    if (user._id) {
+    if (user && user._id) {
       getUserEvents(user._id);
       getUserEventsHosting(user._id);
     }
@@ -21,9 +21,13 @@ const HomePage = () => {
       </Typography>
       {user && user.events && user.eventsHosting && (
         <Fragment>
-          <Typography variant="body1">Events You Are Hosting:</Typography>
+          <Typography variant="body1" style={{ marginTop: "1.5rem" }}>
+            Events You Are Hosting:
+          </Typography>
           <EventsListContainer events={user.eventsHosting} isHosting />
-          <Typography variant="body1">Events You Are Attending:</Typography>
+          <Typography variant="body1" style={{ marginTop: "1.5rem" }}>
+            Events You Are Attending:
+          </Typography>
           <EventsListContainer events={user.events} />
         </Fragment>
       )}
