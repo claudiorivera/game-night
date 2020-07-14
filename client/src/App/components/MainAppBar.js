d0a6fadd80bb414f8fd095b794deea74b80e23f8
Claudio Rivera <me@claudiorivera.com>
2020-07-14 13:43:50 -0500

    Created link arrays and begin styling responsive bar

--- client/src/App/components/MainAppBar.js
+++ client/src/App/components/MainAppBar.js
@@ -1,63 +1,80 @@
-import React, { useContext, Fragment } from "react";
-import { AppBar, Toolbar, Button } from "@material-ui/core";
+import React, { useContext } from "react";
+import { AppBar, Toolbar, Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { UserContext } from "../../pages/user/context";
 
 const useStyles = makeStyles({
   mb3: {
     marginBottom: "3vh",
   },
+  title: {
+    flexGrow: 1,
+    textDecoration: "none",
+    color: "white",
+    fontWeight: 700,
+    fontSize: "1.5rem",
+  },
 });
+
+const adminLinks = [
+  {
+    title: "Games",
+    url: "/games",
+  },
+];
+
+const userLinks = [
+  {
+    title: "Events",
+    url: "/events",
+  },
+  {
+    title: "My Profile",
+    url: "/profile",
+  },
+  {
+    title: "Log Out",
+    url: "/logout",
+  },
+];
 
 const MainAppBar = () => {
   const classes = useStyles();
-  const { user, logoutUser } = useContext(UserContext);
-  const history = useHistory();
+  const { user } = useContext(UserContext);
 
   return (
     <AppBar position="sticky" className={classes.mb3}>
       <Toolbar>
-        <Button component={Link} to="/" color="inherit">
-          Home
-        </Button>
-        {/* Show the Games List link if the user is an admin */}
-        {user && user.isAdmin && (
-          <Button color="inherit" component={Link} to="/games">
-            Games
+        <Typography className={classes.title} component={Link} to="/">
+          Game Night
+        </Typography>
+        {/* Admin links */}
+        {user &&
+          user.isAdmin &&
+          adminLinks.map(({ title, url }, index) => (
+            <Button key={index} color="inherit" component={Link} to={url}>
+              {title}
+            </Button>
+          ))}
+        {/* User links */}
+        {user &&
+          user._id &&
+          userLinks.map(({ title, url }, index) => (
+            <Button key={index} color="inherit" component={Link} to={url}>
+              {title}
+            </Button>
+          ))}
+        {/* Show the Login/Register button if there's no user */}
+        {!user && (
+          <Button
+            className={classes.alignRight}
+            component={Link}
+            to="/login"
+            color="inherit"
+          >
+            Login/Register
           </Button>
-        )}
-        {/* Show the Profile and Log Out links if a non-admin user is logged in */}
-        {user && user._id ? (
-          <Fragment>
-            <Button color="inherit" component={Link} to="/events">
-              Events
-            </Button>
-            <Button color="inherit" component={Link} to="/profile">
-              My Profile
-            </Button>
-            <Button
-              color="inherit"
-              onClick={() => {
-                logoutUser();
-                history.push("/login");
-              }}
-            >
-              Logout
-            </Button>
-          </Fragment>
-        ) : (
-          // Show the Login/Register link if a user is not logged in
-          <Fragment>
-            <Button
-              className={classes.alignRight}
-              component={Link}
-              to="/login"
-              color="inherit"
-            >
-              Login/Register
-            </Button>
-          </Fragment>
         )}
       </Toolbar>
     </AppBar>
