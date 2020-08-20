7bf517a8a3b8748192f07dee29b2de9a145bdd9f
Claudio Rivera <me@claudiorivera.com>
2020-08-20 11:19:11 -0500

    Confirm profile delete

--- client/src/pages/user/ProfilePage.js
+++ client/src/pages/user/ProfilePage.js
@@ -1,6 +1,15 @@
-import React, { useContext, Fragment, useEffect } from "react";
+import React, { useContext, Fragment, useEffect, useState } from "react";
 import { UserContext } from "./context";
-import { Container, Typography, Button } from "@material-ui/core";
+import {
+  Container,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 const moment = require("moment");
 
@@ -17,32 +26,56 @@ const ProfilePage = () => {
     //eslint-disable-next-line
   }, []);
 
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Fragment>
       {user && (
-        <Fragment>
-          <Container>
-            <Typography variant="h5">Name: {user.name}</Typography>
-            <Typography variant="h5">Email: {user.email}</Typography>
-            <Typography variant="h5">
-              Admin? {user.isAdmin ? "Yes" : "No"}
-            </Typography>
-            <Typography variant="h5">
-              Date Created: {moment(user.dateCreated).format("MMMM Do, YYYY")}
-            </Typography>
-          </Container>
-          <Container>
-            <Button
-              fullWidth
-              color="secondary"
-              variant="contained"
-              size="large"
-              onClick={handleDelete}
-            >
-              Delete My Profile
-            </Button>
-          </Container>
-        </Fragment>
+        <Container>
+          <Typography variant="h5">Name: {user.name}</Typography>
+          <Typography variant="h5">Email: {user.email}</Typography>
+          <Typography variant="h5">
+            Admin? {user.isAdmin ? "Yes" : "No"}
+          </Typography>
+          <Typography variant="h5">
+            Date Created: {moment(user.dateCreated).format("MMMM Do, YYYY")}
+          </Typography>
+          <Button
+            fullWidth
+            color="secondary"
+            variant="contained"
+            size="large"
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            Delete My Profile
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Delete Event?"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Are you sure you want to delete your profile?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleDelete} color="primary" autoFocus>
+                Yes
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Container>
       )}
     </Fragment>
   );
