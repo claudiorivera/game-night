8aece08386cee9c20a777563077a986d2b3f7fbb
Claudio Rivera <me@claudiorivera.com>
2020-10-07 13:01:01 -0500

    Add dbConnect

---
+++ util/dbConnect.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+const dbConnect = async () => {
+  if (connection.isConnected) {
+    return;
+  }
+
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+
+  connection.isConnected = db.connections[0].readyState;
+};
+
+export default dbConnect;
