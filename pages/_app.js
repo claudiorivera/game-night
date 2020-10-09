1b25601bde06b4f1e839f05d79685c9509026461
Claudio Rivera <me@claudiorivera.com>
2020-10-09 13:24:43 -0500

    Begin refactoring app components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,45 @@
+import MomentUtils from "@date-io/moment";
+import { CssBaseline } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import AlertDialog from "../components/AlertDialog";
+import { EventsProvider } from "../components/events/context";
+import { GamesProvider } from "../components/games/context";
+import MainAppBar from "../components/MainAppBar";
+import { UserProvider } from "../components/user/context";
+import { AppProvider } from "../context";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2A9D8F",
+    },
+    secondary: {
+      main: "#E9C46A",
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
+
+const App = ({ Component, pageProps }) => (
+  <AppProvider>
+    <UserProvider>
+      <GamesProvider>
+        <EventsProvider>
+          <ThemeProvider theme={theme}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <CssBaseline />
+              <MainAppBar />
+              <AlertDialog />
+              <Component {...pageProps} />
+            </MuiPickersUtilsProvider>
+          </ThemeProvider>
+        </EventsProvider>
+      </GamesProvider>
+    </UserProvider>
+  </AppProvider>
+);
 
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from "next/head";
 import React from "react";
 import "fontsource-roboto";
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-// App components
-import AlertDialog from "../components/AlertDialog";
-import MainAppBar from "../components/MainAppBar";
+
 import PrivateRoute from "../components/PrivateRoute";
 // Pages
 import LoginPage from "../components/user/LoginPage";
@@ -18,83 +14,49 @@ import EventsListPage from "../components/events/EventsListPage";
 import EventDetailsPage from "../components/events/EventDetailsPage";
 import AddEventPage from "../components/events/AddEventPage";
 import EditEventPage from "../components/events/EditEventPage";
-// Contexts
-import { AppProvider } from "../context";
-import { UserProvider } from "../components/user/context";
-import { GamesProvider } from "../components/games/context";
-import { EventsProvider } from "../components/events/context";
+
 // Util
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import MomentUtils from "@date-io/moment";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#2A9D8F",
-    },
-    secondary: {
-      main: "#E9C46A",
-    },
-    contrastThreshold: 3,
-    tonalOffset: 0.2,
-  },
-});
 
 const Home = () => (
   <div>
     <Head>
       <title>Game Night</title>
     </Head>
-    <AppProvider>
-      <UserProvider>
-        <GamesProvider>
-          <EventsProvider>
-            <ThemeProvider theme={theme}>
-              <MuiPickersUtilsProvider utils={MomentUtils}>
-                <Router>
-                  <CssBaseline />
-                  <MainAppBar />
-                  <AlertDialog />
-                  <Switch>
-                    <Route exact path="/">
-                      <HomePage />
-                    </Route>
-                    <PrivateRoute exact path="/games">
-                      <GamesListPage />
-                    </PrivateRoute>
-                    <PrivateRoute exact path="/events">
-                      <EventsListPage />
-                    </PrivateRoute>
-                    <PrivateRoute exact path="/events/add">
-                      <AddEventPage />
-                    </PrivateRoute>
-                    <PrivateRoute path="/events/:eventId/edit">
-                      <EditEventPage />
-                    </PrivateRoute>
-                    <PrivateRoute path="/events/:eventId">
-                      <EventDetailsPage />
-                    </PrivateRoute>
-                    <PrivateRoute path="/games/add">
-                      <AddGamePage />
-                    </PrivateRoute>
-                    <PrivateRoute path="/profile">
-                      <ProfilePage />
-                    </PrivateRoute>
-                    <Route path="/login">
-                      <LoginPage />
-                    </Route>
-                    <Route path="/logout">
-                      <LogoutPage />
-                    </Route>
-                  </Switch>
-                </Router>
-              </MuiPickersUtilsProvider>
-            </ThemeProvider>
-          </EventsProvider>
-        </GamesProvider>
-      </UserProvider>
-    </AppProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <PrivateRoute exact path="/games">
+          <GamesListPage />
+        </PrivateRoute>
+        <PrivateRoute exact path="/events">
+          <EventsListPage />
+        </PrivateRoute>
+        <PrivateRoute exact path="/events/add">
+          <AddEventPage />
+        </PrivateRoute>
+        <PrivateRoute path="/events/:eventId/edit">
+          <EditEventPage />
+        </PrivateRoute>
+        <PrivateRoute path="/events/:eventId">
+          <EventDetailsPage />
+        </PrivateRoute>
+        <PrivateRoute path="/games/add">
+          <AddGamePage />
+        </PrivateRoute>
+        <PrivateRoute path="/profile">
+          <ProfilePage />
+        </PrivateRoute>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/logout">
+          <LogoutPage />
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 export default Home;
