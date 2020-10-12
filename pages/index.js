a490844f59625209e6b83efbff91505708c94edb
Claudio Rivera <me@claudiorivera.com>
2020-10-12 11:22:22 -0500

    Move home page component to index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,41 @@
+import { Container, Typography } from "@material-ui/core";
+import Link from "next/link";
 import React from "react";
-import HomePage from "../components/home/HomePage";
+import EventsListContainer from "../components/events/components/EventsListContainer";
+import useRequest from "../util/useRequest";
 
-const index = () => <HomePage />;
+const HomePage = () => {
+  const { user } = useRequest({
+    url: "/api/user/auth",
+  });
 
-export default index;
+  return (
+    <Container>
+      {!user && (
+        <Typography variant="h4">
+          Hello, there. Please <Link href="/login">login or register</Link> to
+          continue.
+        </Typography>
+      )}
+      {user?._id && `Hello, ${user.name}.`}
+      {user?.eventsHosting && (
+        <Fragment>
+          <Typography variant="body1" style={{ marginTop: "1.5rem" }}>
+            Events You Are Hosting:
+          </Typography>
+          <EventsListContainer events={user.eventsHosting} isHosting />
+        </Fragment>
+      )}
+      {user?.events && (
+        <Fragment>
+          <Typography variant="body1" style={{ marginTop: "1.5rem" }}>
+            Events You Are Attending:
+          </Typography>
+          <EventsListContainer events={user.events} />
+        </Fragment>
+      )}
+    </Container>
+  );
+};
+
+export default HomePage;
