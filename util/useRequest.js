bb0cd02e1160e968dcb6d20918803007c57229c2
Claudio Rivera <me@claudiorivera.com>
2020-10-12 10:35:59 -0500

    Added useRequest hook

---
+++ util/useRequest.js
@@ -0,0 +1,19 @@
+// https://github.com/vercel/swr/blob/master/examples/axios/libs/useRequest.js
+import useSWR from "swr";
+import axios from "axios";
+
+export default function useRequest(request, { initialData, ...config } = {}) {
+  return useSWR(
+    request && JSON.stringify(request),
+    () => axios(request || {}).then((response) => response.data),
+    {
+      ...config,
+      initialData: initialData && {
+        status: 200,
+        statusText: "InitialData",
+        headers: {},
+        data: initialData,
+      },
+    }
+  );
+}
