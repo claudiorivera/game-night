2f8a12ed24de9e2a4d232170c88e4c8e04709244
Claudio Rivera <me@claudiorivera.com>
2020-10-15 08:32:06 -0500

    Added passport back in

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,9 @@
 import nextConnect from "next-connect";
+import passport from "../util/passport";
 import db from "./db";
 
 const middleware = nextConnect();
 
-middleware.use(db);
+middleware.use(db).use(passport.initialize()).use(passport.session());
 
 export default middleware;
---
+++ util/passport.js
@@ -0,0 +1,8 @@
+import passport from "passport";
+import User from "../models/User";
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+export default passport;
