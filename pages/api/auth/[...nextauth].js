ece0acad1a6dddb8d694d8cfc014ed2a163ae67c
Claudio Rivera <me@claudiorivera.com>
2020-10-15 07:43:08 -0500

    Switch providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,9 +3,32 @@ import Providers from "next-auth/providers";
 
 const options = {
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Email and Password",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "me@gmail.com" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        console.log(`credentials: ${JSON.stringify(credentials)}`);
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return Promise.resolve(user);
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return Promise.resolve(null);
+          // You can also Reject this callback with an Error or with a URL:
+          // return Promise.reject(new Error('error message')) // Redirect to error page
+          // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+        }
+      },
     }),
   ],
   database: process.env.MONGODB_URI,
