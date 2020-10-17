6de4538665128d74283c7e39d503a451831dc400
Claudio Rivera <me@claudiorivera.com>
2020-10-16 19:37:15 -0500

    Add Facebook login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,6 +14,10 @@ handler.use((req, res) =>
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
       }),
+      Providers.Facebook({
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      }),
     ],
     database: process.env.MONGODB_URI,
     secret: process.env.SECRET,
