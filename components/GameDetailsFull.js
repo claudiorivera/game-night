37aebffd69f4c5d998f3cb1dcb6b22955cfed5fb
Claudio Rivera <me@claudiorivera.com>
2020-10-16 20:03:27 -0500

    Tweaked breakpoints

--- components/GameDetailsFull.js
+++ components/GameDetailsFull.js
@@ -5,11 +5,11 @@ import GameMetaData from "./GameMetaData";
 const GameDetailsFull = ({ game }) => {
   return game ? (
     <Grid container spacing={4}>
-      <Grid item md={5} sm={3} xs={12}>
+      <Grid item md={5} sm={4} xs={12}>
         <img src={game.imageSrc} alt={game.name} style={{ width: "100%" }} />
         <GameMetaData game={game} />
       </Grid>
-      <Grid item md={7} sm={9} xs={12}>
+      <Grid item md={7} sm={8} xs={12}>
         <Typography variant="body1">{game.description}</Typography>
       </Grid>
     </Grid>
