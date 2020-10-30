51467ed5c96948016d9caa09b187102303bf40f8
Claudio Rivera <me@claudiorivera.com>
2020-10-29 19:41:15 -0500

    Global components

---
+++ components/StyledButton.js
@@ -0,0 +1,7 @@
+const { styled, Button } = require("@material-ui/core");
+
+const StyledButton = styled(Button)({
+  margin: ".5rem 0",
+});
+
+export default StyledButton;
---
+++ components/StyledDivider.js
@@ -0,0 +1,7 @@
+import { Divider, styled } from "@material-ui/core";
+
+const StyledDivider = styled(Divider)({
+  margin: "1.5rem",
+});
+
+export default StyledDivider;
