acf8b05328d646635f38b93bd7c245d34821e784
Claudio Rivera <me@claudiorivera.com>
2023-02-09 21:25:03 -0600

    🚧 wip

--- lib/bggFetchGameById.ts
+++ lib/bggFetchGameById.ts
@@ -1,36 +1,144 @@
 // Interfaces with the BoardGameGeek API2 and returns a custom game object
 import axios from "axios";
-import { XMLParser } from "fast-xml-parser";
-import { BGGGameResponse, BGGQueryResponse } from "types";
+import { BGGQueryResponse } from "types";
+import { z } from "zod";
+
+import { xmlParser } from "./xmlParser";
+
+const numberValueSchema = z.object({ value: z.number() });
+
+const nameSchema = z.object({
+  type: z.enum(["primary", "alternate"]),
+  sortindex: z.number(),
+  value: z.string(),
+});
+
+const linkSchema = z.object({
+  type: z.enum([
+    "boardgamecategory",
+    "boardgamemechanic",
+    "boardgamefamily",
+    "boardgameexpansion",
+    "boardgamedesigner",
+    "boardgameartist",
+    "boardgamepublisher",
+    "boardgameimplementation",
+  ]),
+  id: z.number(),
+  value: z.string(),
+});
+
+const rankSchema = z.object({
+  type: z.string(),
+  id: z.number(),
+  name: z.enum(["boardgame", "strategygames", "familygames"]),
+  friendlyname: z.string(),
+  value: z.string().or(z.number()),
+  bayesaverage: z.number().or(z.string()),
+});
+
+const pollResultsSchema = z
+  .object({
+    numplayers: z.string().or(z.number()).nullish(),
+    result: z
+      .array(
+        z.object({
+          value: z.number().or(z.string()),
+          numvotes: z.number(),
+        })
+      )
+      .nullish(),
+  })
+  .nullish();
+
+const schema = z.object({
+  "?xml": z.object({ version: z.number(), encoding: z.string() }),
+  items: z.object({
+    item: z.object({
+      thumbnail: z.string().url().nullish(),
+      image: z.string().url().nullish(),
+      name: nameSchema.or(z.array(nameSchema)),
+      description: z.string(),
+      yearpublished: numberValueSchema,
+      minplayers: numberValueSchema,
+      maxplayers: numberValueSchema,
+      poll: z.array(
+        z.object({
+          name: z.string(),
+          title: z.string(),
+          totalvotes: z.number(),
+          results: z.array(pollResultsSchema).or(pollResultsSchema),
+        })
+      ),
+      playingtime: numberValueSchema,
+      minplaytime: numberValueSchema,
+      maxplaytime: numberValueSchema,
+      minage: numberValueSchema,
+      link: z.array(linkSchema),
+      statistics: z.object({
+        ratings: z.object({
+          usersrated: numberValueSchema,
+          average: numberValueSchema,
+          bayesaverage: numberValueSchema,
+          ranks: z.object({
+            rank: z.array(rankSchema).or(rankSchema),
+          }),
+          stddev: numberValueSchema,
+          median: numberValueSchema,
+          owned: numberValueSchema,
+          trading: numberValueSchema,
+          wanting: numberValueSchema,
+          wishing: numberValueSchema,
+          numcomments: numberValueSchema,
+          numweights: numberValueSchema,
+          averageweight: numberValueSchema,
+        }),
+        page: z.number(),
+      }),
+      type: z.enum(["boardgame", "boardgameexpansion"]),
+      id: z.number(),
+    }),
+    termsofuse: z.string(),
+  }),
+});
+
+// https://boardgamegeek.com/wiki/page/BGG_XML_API2
+const buildUrlForGameId = (id: number) => {
+  const url = new URL("https://api.geekdo.com/xmlapi2/thing");
+
+  url.searchParams.append("id", String(id));
+  url.searchParams.append("stats", String(1));
+
+  return url.toString();
+};
 
 export const bggFetchGameById = async (id: number) => {
-  const { data } = await axios.get<string>(
-    // https://boardgamegeek.com/wiki/page/BGG_XML_API2
-    `https://api.geekdo.com/xmlapi2/thing?id=${id}&stats=1`
-  );
+  console.log("hello?");
+  const url = buildUrlForGameId(id);
 
-  const parser = new XMLParser({
-    ignoreAttributes: false,
-    attributeNamePrefix: "",
-  });
+  const { data: xml } = await axios.get(url);
 
-  const parsedData = parser.parse(data);
+  const parsedData = xmlParser.parse(xml);
 
-  const game = parsedData.items.item;
+  console.log({ parsedData });
+
+  const validData = schema.parse(parsedData);
+
+  const game = validData.items.item;
 
   // Clean up the api data and set our new game object
-  const gameObject: BGGGameResponse = {
-    bggId: +game.id,
+  const gameObject = {
+    bggId: game.id,
     imageSrc: game.image,
     thumbnailSrc: game.thumbnail,
     description: game.description,
-    yearPublished: +game.yearpublished.value,
-    minPlayers: +game.minplayers.value,
-    maxPlayers: +game.maxplayers.value,
-    playingTime: +game.playingtime.value,
-    minAge: +game.minage.value,
-    rating: +game.statistics.ratings.average.value,
-    numOfRatings: +game.statistics.ratings.usersrated.value,
+    yearPublished: game.yearpublished.value,
+    minPlayers: game.minplayers.value,
+    maxPlayers: game.maxplayers.value,
+    playingTime: game.playingtime.value,
+    minAge: game.minage.value,
+    rating: game.statistics.ratings.average.value,
+    numOfRatings: game.statistics.ratings.usersrated.value,
     // Ignore all alternate names, if there are multiple (ie. isArray)
     name: Array.isArray(game.name)
       ? game.name.filter(
@@ -55,5 +163,6 @@ export const bggFetchGameById = async (id: number) => {
       )
       .map((mechanic: BGGQueryResponse) => mechanic.value),
   };
+
   return gameObject;
 };
--- lib/bggFetchGamesByQuery.ts
+++ lib/bggFetchGamesByQuery.ts
@@ -1,29 +1,66 @@
 // Interfaces with the BoardGameGeek API2 and returns a list of games from a query
 // by calling the bggFetchGameById function for each result
 import axios from "axios";
-import { XMLParser } from "fast-xml-parser";
+import { z } from "zod";
 
 import { bggFetchGameById } from "./bggFetchGameById";
+import { xmlParser } from "./xmlParser";
 
-const API_CALL_LIMIT = 10;
+const API_CALL_LIMIT = 1;
+
+const gameResultSchema = z.object({
+  id: z.number(),
+  type: z.literal("boardgame"),
+  name: z.object({
+    type: z.enum(["primary", "alternate"]),
+    value: z.string(),
+  }),
+  yearpublished: z
+    .object({
+      value: z.number(),
+    })
+    .nullish(),
+});
+
+const itemSchema = z.array(gameResultSchema).or(gameResultSchema);
+
+const parsedDataSchema = z.object({
+  "?xml": z.object({
+    encoding: z.string(),
+    version: z.number(),
+  }),
+  items: z.object({
+    termsofuse: z.string(),
+    total: z.number(),
+    item: itemSchema.nullish(),
+  }),
+});
+
+// https://boardgamegeek.com/wiki/page/BGG_XML_API2
+const buildUrlForGameQuery = (query: string) => {
+  const url = new URL("https://api.geekdo.com/xmlapi2/search");
+
+  url.searchParams.append("query", query);
+  url.searchParams.append("type", "boardgame");
+
+  return url.toString();
+};
 
 export const bggFetchGamesByQuery = async (query: string) => {
-  const { data } = await axios.get<string>(
-    // https://boardgamegeek.com/wiki/page/BGG_XML_API2
-    `https://api.geekdo.com/xmlapi2/search?query=${query}&type=boardgame`
-  );
+  const url = buildUrlForGameQuery(query);
 
-  const parser = new XMLParser({
-    attributeNamePrefix: "",
-    ignoreAttributes: false,
-    parseAttributeValue: true,
-  });
+  const { data: xml } = await axios.get(url);
 
-  const parsedData = parser.parse(data);
+  const parsedData = xmlParser.parse(xml);
 
-  const gameList = parsedData.items.item;
+  const validData = parsedDataSchema.parse(parsedData);
+
+  const gameList = validData.items.item;
+
+  if (!gameList) return [];
 
   const results = [];
+
   if (Array.isArray(gameList)) {
     // Limit the number of calls to the BGG API, so it doesn't yell at us
     for (const game of gameList.slice(0, API_CALL_LIMIT)) {
