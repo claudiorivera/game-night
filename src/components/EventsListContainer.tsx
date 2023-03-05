d58bfb38eede72b0b62805ffc1bdea67caae03bf
Claudio Rivera <me@claudiorivera.com>
2023-03-04 20:14:59 -0600

    ✨ Migrate to tRPC
    🚧 Add user router and refactor auth
    
    🚧 wip
    
    🚧 Get datepicker working
    
    🔥 Remove unused api endpoints and next-connect dependency
    
    🚧 wip
    
    🏷️ Update types
    
    💚 Fix env vars
    
    💚 Fix env vars
    
    💚 Fix env var
    
    🚧 Swap out moment for dayjs and tidy up
    
    🚧 Use shared prisma client
    
    🚧 Move faker from devDeps to deps
    
    💚 Fix mui

---
+++ src/components/EventsListContainer.tsx
@@ -0,0 +1,26 @@
+import { Grid } from "@mui/material";
+
+import { EventSummaryCard } from "~/components";
+import { PopulatedEvent } from "~/lib/prismaHelpers";
+
+type EventsListContainerProps = {
+  events: PopulatedEvent[];
+  isHosting?: boolean;
+  [props: string]: any;
+};
+
+export const EventsListContainer = ({
+  events,
+  isHosting,
+  ...props
+}: EventsListContainerProps) => {
+  return (
+    <Grid container spacing={2} {...props}>
+      {events.map((event) => (
+        <Grid item key={event.id} xs={12} sm={6} md={3}>
+          <EventSummaryCard event={event} isHosting={isHosting} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
