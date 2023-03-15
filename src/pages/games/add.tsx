0f1a1f32ecd9f5fecd43b02572b4ac63e7d5d20c
Claudio Rivera <me@claudiorivera.com>
2023-03-14 22:12:35 -0500

    🩹 Fix inconsistent usage of Container

--- src/pages/games/add.tsx
+++ src/pages/games/add.tsx
@@ -4,13 +4,12 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
-  Container,
   TextField,
   Typography,
 } from "@mui/material";
 import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
 import { getServerSession } from "next-auth";
+import { useRouter } from "next/router";
 import { useContext, useState } from "react";
 
 import { GameDetails } from "~/components";
@@ -71,94 +70,86 @@ const AddGamePage = () => {
 
   return (
     <>
-      <Container>
-        <form onSubmit={handleSearch}>
-          <TextField
-            name="query"
-            id="query"
-            label="Search for game to add"
-            placeholder="Enter a boardgame name to search for"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-            value={query}
-            onChange={handleQueryChange}
-          />
-          <LoadingButton
-            variant="contained"
-            color="secondary"
-            fullWidth
-            type="submit"
-            disabled={isFetching}
-            loading={isFetching}
-          >
-            Search
-          </LoadingButton>
-        </form>
-      </Container>
-      <Container>
-        <>
-          {queryResults
-            .filter(
-              (result): result is NonNullable<BGGGameResponse> => !!result
-            )
-            .map((result) => (
-              <Accordion key={result.bggId} square>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls={`panel-${result.bggId}-content`}
-                >
-                  <Typography>
-                    {result.name} ({result.yearPublished})
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <LoadingButton
-                    sx={{
-                      my: 1,
-                    }}
-                    fullWidth
-                    size="large"
-                    color="secondary"
-                    variant="contained"
-                    disabled={disabled}
-                    loading={disabled}
-                    onClick={() => {
-                      addGame({
-                        bggId: result.bggId,
-                        imageSrc: result.imageSrc,
-                        thumbnailSrc: result.thumbnailSrc,
-                        description: result.description,
-                        yearPublished: result.yearPublished,
-                        minPlayers: result.minPlayers,
-                        maxPlayers: result.maxPlayers,
-                        playingTime: result.playingTime,
-                        minAge: result.minAge,
-                        rating: result.rating,
-                        numOfRatings: result.numOfRatings,
-                        name: result.name,
-                        authors: result.authors,
-                        categories: result.categories,
-                        mechanics: result.mechanics,
-                      });
-                    }}
-                  >
-                    Add This Game
-                  </LoadingButton>
-                  <GameDetails game={result} />
-                </AccordionDetails>
-              </Accordion>
-            ))}
-        </>
-      </Container>
+      <form onSubmit={handleSearch}>
+        <TextField
+          name="query"
+          id="query"
+          label="Search for game to add"
+          placeholder="Enter a boardgame name to search for"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          value={query}
+          onChange={handleQueryChange}
+        />
+        <LoadingButton
+          variant="contained"
+          color="secondary"
+          fullWidth
+          type="submit"
+          disabled={isFetching}
+          loading={isFetching}
+        >
+          Search
+        </LoadingButton>
+      </form>
+      {queryResults
+        .filter((result): result is NonNullable<BGGGameResponse> => !!result)
+        .map((result) => (
+          <Accordion key={result.bggId} square>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel-${result.bggId}-content`}
+            >
+              <Typography>
+                {result.name} ({result.yearPublished})
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <LoadingButton
+                sx={{
+                  my: 1,
+                }}
+                fullWidth
+                size="large"
+                color="secondary"
+                variant="contained"
+                disabled={disabled}
+                loading={disabled}
+                onClick={() => {
+                  addGame({
+                    bggId: result.bggId,
+                    imageSrc: result.imageSrc,
+                    thumbnailSrc: result.thumbnailSrc,
+                    description: result.description,
+                    yearPublished: result.yearPublished,
+                    minPlayers: result.minPlayers,
+                    maxPlayers: result.maxPlayers,
+                    playingTime: result.playingTime,
+                    minAge: result.minAge,
+                    rating: result.rating,
+                    numOfRatings: result.numOfRatings,
+                    name: result.name,
+                    authors: result.authors,
+                    categories: result.categories,
+                    mechanics: result.mechanics,
+                  });
+                }}
+              >
+                Add This Game
+              </LoadingButton>
+              <GameDetails game={result} />
+            </AccordionDetails>
+          </Accordion>
+        ))}
     </>
   );
 };
