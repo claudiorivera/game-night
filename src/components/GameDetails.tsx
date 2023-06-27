8e4e06e7cbf9804fa74643cca09325a3f1ab8f85
Claudio Rivera <me@claudiorivera.com>
2023-06-26 20:39:30 -0500

    ♻️ Simplify components

--- src/components/GameDetails.tsx
+++ src/components/GameDetails.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 
-import { GameMetaData } from "~/components";
 import { type BGGGameResponse } from "~/server/api/routers/bgg";
 
 type Props = {
@@ -21,7 +20,25 @@ export const GameDetails = ({ game }: Props) => {
 						className="object-contain"
 					/>
 				</div>
-				<GameMetaData game={game} />
+				<div className="flex flex-col gap-2">
+					<BadgesDisplay label="Authors" badges={game.authors} />
+					<BadgesDisplay label="Categories" badges={game.categories} />
+					<Description
+						term="Average BGG Rating"
+						definition={`${game.rating.toFixed(2)} (${
+							game.numOfRatings
+						} ratings)`}
+					/>
+					<Description
+						term="Players"
+						definition={`${game.minPlayers} to ${game.maxPlayers}`}
+					/>
+					<Description
+						term="Playing Time"
+						definition={`${game.playingTime} minutes`}
+					/>
+					<Description term="Ages" definition={`${game.minAge}+`} />
+				</div>
 			</div>
 			<div className="col-span-12 sm:col-span-8">
 				<p>{game.description}</p>
@@ -29,3 +46,33 @@ export const GameDetails = ({ game }: Props) => {
 		</div>
 	);
 };
+
+type DescriptionProps = {
+	term: string;
+	definition: string;
+};
+
+const Description = ({ term, definition }: DescriptionProps) => (
+	<div className="flex items-center gap-2">
+		<dt>{term}:</dt>
+		<dd>{definition}</dd>
+	</div>
+);
+
+type BadgesDisplayProps = {
+	label: string;
+	badges: string[];
+};
+
+const BadgesDisplay = ({ label, badges }: BadgesDisplayProps) => (
+	<div className="flex flex-wrap gap-2">
+		<span>{label}: </span>
+		<div className="flex flex-wrap gap-1">
+			{badges.map((badge) => (
+				<div className="badge badge-neutral" key={badge}>
+					{badge}
+				</div>
+			))}
+		</div>
+	</div>
+);
--- src/pages/games/add.tsx
+++ src/pages/games/add.tsx
@@ -2,7 +2,7 @@ import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
 import { toast } from "react-hot-toast";
 
 import { GameDetails } from "~/components";
@@ -10,8 +10,70 @@ import { api } from "~/lib/api";
 import { type BGGGameResponse } from "~/server/api/routers/bgg";
 
 const AddGamePage = () => {
-	const router = useRouter();
 	const [query, setQuery] = useState("");
+	const { data: results, isFetching } = api.bgg.gamesByQuery.useQuery(query, {
+		enabled: !!query,
+	});
+
+	return (
+		<div className="container mx-auto">
+			<QueryInput setQuery={setQuery} disabled={isFetching} />
+			<QueryResults results={results} />
+		</div>
+	);
+};
+
+export default AddGamePage;
+
+type QueryInputProps = {
+	setQuery: Dispatch<SetStateAction<string>>;
+	disabled: boolean;
+};
+
+const QueryInput = ({ setQuery, disabled }: QueryInputProps) => (
+	<form
+		className="flex flex-col gap-2 pb-4"
+		onSubmit={(e) => {
+			e.preventDefault();
+			const q = new FormData(e.currentTarget).get("query");
+
+			if (typeof q === "string") {
+				setQuery(q);
+			}
+		}}
+	>
+		<input
+			name="query"
+			id="query"
+			type="text"
+			placeholder="Enter a boardgame name to search for"
+			className="input-bordered input"
+			defaultValue=""
+		/>
+		<button className="btn-secondary btn" type="submit" disabled={disabled}>
+			Search
+		</button>
+	</form>
+);
+
+type QueryResultsProps = {
+	results?: Array<BGGGameResponse>;
+};
+
+const QueryResults = ({ results }: QueryResultsProps) => {
+	if (!results) return null;
+
+	return results.map((result) => (
+		<QueryResult key={result.bggId} result={result} />
+	));
+};
+
+type QueryResultProps = {
+	result: BGGGameResponse;
+};
+
+const QueryResult = ({ result }: QueryResultProps) => {
+	const router = useRouter();
 
 	const { mutate: addGame, isLoading: disabled } = api.game.import.useMutation({
 		onSuccess: () => {
@@ -23,95 +85,52 @@ const AddGamePage = () => {
 		},
 	});
 
-	const { data: games, isFetching } = api.bgg.gamesByQuery.useQuery(query, {
-		enabled: !!query,
-	});
-
 	return (
-		<>
-			<div className="container mx-auto">
-				<form
-					className="flex flex-col gap-2 pb-4"
-					onSubmit={(e) => {
-						e.preventDefault();
-						const q = new FormData(e.currentTarget).get("query");
-
-						if (typeof q === "string") {
-							setQuery(q);
-						}
-					}}
-				>
-					<input
-						name="query"
-						id="query"
-						type="text"
-						placeholder="Enter a boardgame name to search for"
-						className="input-bordered input"
-						defaultValue=""
-					/>
-					<button
-						className="btn-secondary btn"
-						type="submit"
-						disabled={isFetching}
-					>
-						Search
-					</button>
-				</form>
-			</div>
-
-			{!!games &&
-				games
-					.filter((result): result is NonNullable<BGGGameResponse> => !!result)
-					.map((result) => (
-						<Disclosure key={result.bggId}>
-							{({ open }) => (
-								<>
-									<Disclosure.Button className="flex w-full justify-between border-b p-4 text-left font-medium hover:bg-slate-200 focus:outline-none focus-visible:ring focus-visible:ring-slate-500 focus-visible:ring-opacity-75">
-										<p>
-											{result.name} ({result.yearPublished})
-										</p>
-										<ChevronUpIcon
-											className={clsx("h-5 w-5", {
-												"rotate-180 transform": open,
-											})}
-										/>
-									</Disclosure.Button>
-									<Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-										<div className="pb-2">
-											<button
-												className="btn-secondary btn w-full"
-												disabled={disabled}
-												onClick={() => {
-													addGame({
-														bggId: result.bggId,
-														imageSrc: result.imageSrc,
-														thumbnailSrc: result.thumbnailSrc,
-														description: result.description,
-														yearPublished: result.yearPublished,
-														minPlayers: result.minPlayers,
-														maxPlayers: result.maxPlayers,
-														playingTime: result.playingTime,
-														minAge: result.minAge,
-														rating: result.rating,
-														numOfRatings: result.numOfRatings,
-														name: result.name,
-														authors: result.authors,
-														categories: result.categories,
-														mechanics: result.mechanics,
-													});
-												}}
-											>
-												Add This Game
-											</button>
-										</div>
-										<GameDetails game={result} />
-									</Disclosure.Panel>
-								</>
-							)}
-						</Disclosure>
-					))}
-		</>
+		<Disclosure>
+			{({ open }) => (
+				<>
+					<Disclosure.Button className="flex w-full justify-between border-b p-4 text-left font-medium hover:bg-slate-200 focus:outline-none focus-visible:ring focus-visible:ring-slate-500 focus-visible:ring-opacity-75">
+						<p>
+							{result.name} ({result.yearPublished})
+						</p>
+						<ChevronUpIcon
+							className={clsx("h-5 w-5", {
+								"rotate-180 transform": open,
+							})}
+						/>
+					</Disclosure.Button>
+					<Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+						<div className="pb-2">
+							<button
+								className="btn-secondary btn w-full"
+								disabled={disabled}
+								onClick={() => {
+									addGame({
+										bggId: result.bggId,
+										imageSrc: result.imageSrc,
+										thumbnailSrc: result.thumbnailSrc,
+										description: result.description,
+										yearPublished: result.yearPublished,
+										minPlayers: result.minPlayers,
+										maxPlayers: result.maxPlayers,
+										playingTime: result.playingTime,
+										minAge: result.minAge,
+										rating: result.rating,
+										numOfRatings: result.numOfRatings,
+										name: result.name,
+										authors: result.authors,
+										categories: result.categories,
+										mechanics: result.mechanics,
+									});
+								}}
+							>
+								Add This Game
+							</button>
+						</div>
+						<GameDetails game={result} />
+					</Disclosure.Panel>
+				</>
+			)}
+		</Disclosure>
 	);
 };
-
-export default AddGamePage;
