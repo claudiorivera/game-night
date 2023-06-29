4d84268ea503ce1f2e5de0cfd21be5bb8bbd602f
Claudio Rivera <me@claudiorivera.com>
2023-06-29 18:44:56 -0500

    💄 Update sign-up page styles for consistency

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,17 +1,21 @@
 import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => (
-	<div style={styles}>
-		<SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+	<div className="flex justify-center p-4">
+		<SignUp
+			path="/sign-up"
+			routing="path"
+			signInUrl="/sign-in"
+			appearance={{
+				elements: {
+					formButtonPrimary: "btn-secondary btn w-full",
+				},
+				layout: {
+					logoPlacement: "none",
+				},
+			}}
+		/>
 	</div>
 );
 
 export default SignUpPage;
-
-const styles = {
-	width: "100vw",
-	height: "100vh",
-	display: "flex",
-	justifyContent: "center",
-	alignItems: "center",
-};
