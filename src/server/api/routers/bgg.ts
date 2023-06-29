a3442d6739072db678d83f92bd704b72bf19acca
Claudio Rivera <me@claudiorivera.com>
2023-06-28 21:51:00 -0500

    🐛 Parse special characters

--- src/server/api/routers/bgg.ts
+++ src/server/api/routers/bgg.ts
@@ -170,7 +170,10 @@ export const parsedBggGameSchema = z
 								case "boardgamedesigner":
 									return {
 										...acc,
-										authors: [...acc.authors, cur.value],
+										authors: [
+											...acc.authors,
+											parseSpecialCharacters(cur.value),
+										],
 									};
 
 								default:
@@ -193,7 +196,7 @@ export const parsedBggGameSchema = z
 		bggId: data.items.item.id,
 		imageSrc: data.items.item.image,
 		thumbnailSrc: data.items.item.thumbnail,
-		description: data.items.item.description,
+		description: parseSpecialCharacters(data.items.item.description),
 		yearPublished: data.items.item.yearpublished,
 		minPlayers: data.items.item.minplayers,
 		maxPlayers: data.items.item.maxplayers,
@@ -201,7 +204,7 @@ export const parsedBggGameSchema = z
 		minAge: data.items.item.minage,
 		rating: data.items.item.statistics.ratings.average,
 		numOfRatings: data.items.item.statistics.ratings.usersrated,
-		name: data.items.item.name,
+		name: parseSpecialCharacters(data.items.item.name),
 		categories: data.items.item.link.categories,
 		mechanics: data.items.item.link.mechanics,
 		authors: data.items.item.link.authors,
@@ -210,3 +213,38 @@ export const parsedBggGameSchema = z
 type MetaData = Record<"categories" | "mechanics" | "authors", Array<string>>;
 
 export type BGGGameResponse = RouterOutputs["bgg"]["gameById"];
+
+const parseSpecialCharacters = (str: string) =>
+	str
+		.replace(/&#10;/g, "\n")
+		.replace(/&nbsp;/g, " ")
+		.replace(/&mdash;/, "—")
+		.replace(/&auml;/g, "ä")
+		.replace(/&#226;/g, "â")
+		.replace(/&#128;/g, "€")
+		.replace(/&#147;/g, "“")
+		.replace(/&amp;/g, "&")
+		.replace(/&uuml;/g, "ü")
+		.replace(/&quot;/g, '"')
+		.replace(/&#039;/g, "'")
+		.replace(/&rsquo;/g, "’")
+		.replace(/&ldquo;/g, "“")
+		.replace(/&rdquo;/g, "”")
+		.replace(/&bull;/g, "•")
+		.replace(/&laquo;/g, "«")
+		.replace(/&raquo;/g, "»")
+		.replace(/&hellip;/g, "…")
+		.replace(/&mu;/g, "μ")
+		.replace(/&#207;/g, "Ï")
+		.replace(/&#140;/g, "Œ")
+		.replace(/&nu;/g, "ν")
+		.replace(/&omicron;/g, "ο")
+		.replace(/&sigmaf;/g, "ς")
+		.replace(/&pi;/g, "π")
+		.replace(/&omega;/g, "ω")
+		.replace(/&lambda;/g, "λ")
+		.replace(/&epsilon;/g, "ε")
+		.replace(/&#225;/g, "á")
+		.replace(/&#191;/g, "¿")
+		.replace(/&#150;/g, "–")
+		.replace(/&eacute;/g, "é");
