bfce08fc8cf75959264500d3728df99be3223199
Claudio Rivera <me@claudiorivera.com>
2024-04-19 08:15:20 -0500

    🐛 Fix cron handler

--- src/app/api/cron-handler/route.ts
+++ src/app/api/cron-handler/route.ts
@@ -1,17 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { db } from "~/lib/db";
 import { seed } from "~/lib/seed";
 
-export default async function POST() {
+export async function POST() {
 	const headersList = headers();
 	const authorization = headersList.get("authorization");
 
 	if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
 		await seed(db);
 
-		NextResponse.json(
+		return NextResponse.json(
 			{ message: "Done" },
 			{
 				status: 200,
@@ -19,7 +18,7 @@ export default async function POST() {
 		);
 	}
 
-	NextResponse.json(
+	return NextResponse.json(
 		{ message: "Unauthorized" },
 		{
 			status: 401,
