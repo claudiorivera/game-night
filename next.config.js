c4f1932d3a98cea15b7727d080b2849ed248a55e
Claudio Rivera <me@claudiorivera.com>
2025-02-25 19:42:21 -0600

    👽️ Address breaking change

--- next.config.js
+++ next.config.js
@@ -68,9 +68,6 @@ export default withSentryConfig(config, {
 	// side errors will fail.
 	tunnelRoute: "/monitoring",
 
-	// Hides source maps from generated client bundles
-	hideSourceMaps: true,
-
 	// Automatically tree-shake Sentry logger statements to reduce bundle size
 	disableLogger: true,
 
