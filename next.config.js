5c88c0202e904177465589acc331ccd2f9c8c894
Claudio Rivera <me@claudiorivera.com>
2025-03-04 21:38:03 -0600

    🚨 Address console warning

--- next.config.js
+++ next.config.js
@@ -11,17 +11,6 @@ await import("./src/env.js");
 const config = {
 	transpilePackages: ["next-auth"],
 	reactStrictMode: true,
-
-	/**
-	 * If you have the "experimental: { appDir: true }" setting enabled, then you
-	 * must comment the below `i18n` config out.
-	 *
-	 * @see https://github.com/vercel/next.js/issues/41980
-	 */
-	i18n: {
-		locales: ["en"],
-		defaultLocale: "en",
-	},
 	images: {
 		remotePatterns: [
 			{
