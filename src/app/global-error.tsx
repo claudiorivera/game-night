5cf64473f82fd6d25666d4df822b05aa76b9f3fd
Claudio Rivera <me@claudiorivera.com>
2025-04-13 21:42:58 -0500

    🔥 Remove Sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
-import { useEffect } from "react";
-
 export default function GlobalError({
-	error,
 	reset,
 }: { error: Error & { digest?: string }; reset: () => void }) {
-	useEffect(() => {
-		Sentry.captureException(error);
-	}, [error]);
-
 	return (
 		<html lang="en">
 			<body>
